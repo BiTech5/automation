1dd1a45e034520b4c90c4d375c96bfbb0a3ea807
BiTech5 <bikikc7@gmail.com>
2024-07-02 14:34:59 +0545

    update

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import selenium
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 path="c:\\Program Files (x86)\\chromedriver.exe"
 driver=webdriver.Chrome(path)
 url=r"https://github.com/BiTech5"
 driver.get(url)
-# for closing the tab
+search=driver.find_elements(By.TAG_NAME,"h2")
+print(search)
 print(driver.title)
 driver.quit()
-
-
-# for closing the browser
-# driver.quit()
