5ec9dbeeb1b9df9d115082e3779c591f781c58ff
BiTech5 <bikikc7@gmail.com>
2024-07-02 13:54:00 +0545

    interacting with my own github profile

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import selenium
 from selenium import webdriver
-path="c:\Program Files (x86)\chromedriver.exe"
+path="c:\\Program Files (x86)\\chromedriver.exe"
 driver=webdriver.Chrome(path)
+url=r"https://github.com/BiTech5"
+driver.get(url)
+# for closing the tab
+print(driver.title)
+driver.quit()
 
-driver.get("https://google.com")
+
+# for closing the browser
+# driver.quit()
